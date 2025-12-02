be788113f504c9c0ca959f88be8cc2aba66b127e
Dharmendar Singh <dsy99111@gmail.com>
2025-12-02 13:21:54 +0530

    created vite config js file in frontend for like mobile app

--- exercise-tracker-indexeddb/vite.config.js
+++ exercise-tracker-indexeddb/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [svelte()],
-  server: { port: 5173 }
-})
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Progress Tracker",
+        short_name: "Tracker",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
